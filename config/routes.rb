478fe96846b1c607d2e3882db7cb19b00692c84c
Eric M <ericmarchese77@gmail.com>
2016-10-24 14:09:23 -0400

    Add routes for LetsEncrypt verification

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # For LetsEncrypt verification with sabayon
+  if ENV['ACME_KEY'] && ENV['ACME_TOKEN']
+    get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN"] }" =>
+      proc { [200, {}, [ ENV["ACME_KEY"] ] ] }
+  else
+    ENV.each do |var, _|
+      next unless var.start_with?("ACME_TOKEN_")
+      number = var.sub(/ACME_TOKEN_/, '')
+      get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN_#{number}"] }" =>
+        proc { [200, {}, [ ENV["ACME_KEY_#{number}"] ] ] }
+    end
+  end
 
   resources :users
   resources :user_sessions
