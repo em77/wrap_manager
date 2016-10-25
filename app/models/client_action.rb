3bcbfb689d3de5158b296925584fc70f38e8d958
Eric M <ericmarchese77@gmail.com>
2016-10-25 02:24:37 -0400

    Removed method setting default value for wrap_session on ClientAction. Put form for client_actions#new and client_actions#edit into panel.

--- app/models/client_action.rb
+++ app/models/client_action.rb
@@ -3,9 +3,4 @@ class ClientAction < ApplicationRecord
   accepts_nested_attributes_for :client
   enum wrap_session: [:no, :yes]
   enum wrap_action: [:open, :closed, :completed, :non_wrap, :no_change]
-  after_initialize :set_default_wrap_session, if: :new_record?
-
-  def set_default_wrap_session
-    self.wrap_session ||= :no
-  end
 end
