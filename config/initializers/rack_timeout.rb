1dbe4f2cd5510efccdaebfa24822f8fd80d0f60a
Eric M <ericmarchese@protonmail.com>
2025-11-01 16:57:25 -0400

    Update rack timeout

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,4 +1,10 @@
-if Rails.env.production?
-  Rack::Timeout.service_timeout = 25  # seconds
-end
-# Note: rack-timeout is disabled by default in development/test environments
+# rack-timeout 0.7.0 uses environment variables for configuration
+# Set RACK_TIMEOUT_SERVICE_TIMEOUT environment variable (default is 15 seconds)
+# For Heroku, you can set this in your Heroku config:
+# heroku config:set RACK_TIMEOUT_SERVICE_TIMEOUT=25
+# 
+# Alternatively, you can configure it here using ENV:
+# ENV['RACK_TIMEOUT_SERVICE_TIMEOUT'] ||= '25' if Rails.env.production?
+
+# rack-timeout is automatically configured via Rails middleware
+# It reads from RACK_TIMEOUT_SERVICE_TIMEOUT env var (default: 15 seconds)
