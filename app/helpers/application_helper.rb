b8df7b65788a3e56c58437254245faea5d4a228d
Eric M <ericmarchese@protonmail.com>
2025-11-02 00:41:17 -0400

    Use module prepend to override will_paginate and fix Rails 6.1 argument mismatch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,33 @@
+module WillPaginateBootstrapOverride
+  # Override will_paginate to handle Rails 6.1 argument mismatch
+  # Rails 6.1 passes 4 arguments (collection, options, block, template_context)
+  # but will_paginate only expects (collection, options)
+  def will_paginate(collection = nil, options = nil, *extra_args, &block)
+    # Handle case where first argument is actually options hash
+    if collection.is_a?(Hash) && !collection.is_a?(ActiveRecord::Relation)
+      options, collection = collection, nil
+    end
+
+    # Set Bootstrap renderer if not already set
+    options = options.dup if options.is_a?(Hash)
+    options ||= {}
+    options[:renderer] ||= WillPaginate::ActionView::BootstrapLinkRenderer
+
+    # Call the original method from WillPaginate::ViewHelpers with only collection and options
+    # Use method to get the original implementation
+    original = WillPaginate::ViewHelpers.instance_method(:will_paginate)
+    
+    if collection
+      original.bind(self).call(collection, options)
+    else
+      original.bind(self).call(options)
+    end
+  end
+end
+
 module ApplicationHelper
+  prepend WillPaginateBootstrapOverride
+  
   def flash_class(flash_type)
     case flash_type
       when :notice then "alert alert-info"
@@ -7,8 +36,5 @@ module ApplicationHelper
       when :alert then "alert alert-warning"
     end
   end
-
-  # Removed will_paginate override - using global renderer configuration instead
-  # Bootstrap renderer is set in config/initializers/will_paginate.rb
 
 end
